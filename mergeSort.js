833670366ebdbfd2ecf98c544ecbffa807030466
Jasper Alblas <just_jasper@hotmail.com>
2024-09-02 20:07:49 +0200

    mergeSort one

---
+++ mergeSort.js
@@ -0,0 +1,9 @@
+function mergeSort(array) {
+  if (array.length == 1) {
+    return array;
+  } else {
+    //const chunk = array.slice(0, i + chunkSize);
+    mergeSort(array);
+  }
+  return {};
+}
